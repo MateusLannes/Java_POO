df36ed4e586e1e1ec29cc5fda60770395cd67d0f
Mateus <mateuslannes100@gmail.com>
2023-02-16 15:35:10 +0000

    Ajustes em metodos da classe Conta

--- Projetos_Java/Projeto_Banco/src/Conta.java
+++ Projetos_Java/Projeto_Banco/src/Conta.java
@@ -1,16 +1,15 @@
 package Projetos_Java.Projeto_Banco.src;
 
+import javax.print.event.PrintJobListener;
+
 /**
  * @author Mateus Lannes Cunha
  */
 public class Conta {
     String numero;
     Pessoa titular;
-    double saldo;
-    double limite;
+    double saldo, limite;
     
-   
-
 
     double disponivel() {
         /* retorna o saldo disponivel */
@@ -37,21 +36,22 @@ public class Conta {
     void depositar(double valor){
         /* deposita um valor em uma conta */
         this.saldo += valor;
+        System.out.println("Deposito de " + valor + "realizado com sucesso.");
+        System.out.println("Novo saldo: " + this.saldo);
     }
     
 
-    boolean sacar (double valor) {
+    void sacar (double valor) {
         /* Realiza um Saque, verificando se é possível sacar */
         if (valor <= this.disponivel()){
             this.saldo -= valor;
             System.out.println("Saque na conta " + this.numero + " realizado "
-                    + "com sucesso. Novo saldo: " + this.saldo);
-            return true;       
+                                + "com sucesso.");  
+            System.out.println("Novo saldo: " + this.saldo);  
         }
         else {
-            System.out.println("ERRO: SAque na conta " + this.numero + "nao"
-                    + "foi realizado. Valor disponivel: " + this.disponivel());
-            return false;
+            System.out.println("ERRO: nao foi possivel sacar " + valor );
+            System.out.println("Valor disponivel para saque" + this.disponivel());           
         }
     }
 }
