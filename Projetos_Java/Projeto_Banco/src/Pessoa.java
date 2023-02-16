ae116db592fb41e3edd90ecfd0c573c6dca4e4a8
Mateus <mateuslannes100@gmail.com>
2023-02-16 15:25:33 +0000

    adiconando metodos a Classe Pessoa

--- Projetos_Java/Projeto_Banco/src/Pessoa.java
+++ Projetos_Java/Projeto_Banco/src/Pessoa.java
@@ -15,6 +15,16 @@ public class Pessoa {
         this.idade = this.idade + 1;
     }
 
+    void atribuiCPF( String c){
+        /* Altera o valor do atributo CPF */
+        this.cpf = c;
+    }
+
+    int getIdade(){
+        /* retorna o valor da idade da pessoa */
+        return this.idade;
+    }
+
 
 
 }
