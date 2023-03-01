f44b95fbdcb19f083fe66f2f0b35c6427e9f3cba
Mateus <mateuslannes100@gmail.com>
2023-03-01 22:29:30 +0000

    Criando dois construtores

--- Projetos_Java/Projeto_Circulo/src/Circulo.java
+++ Projetos_Java/Projeto_Circulo/src/Circulo.java
@@ -2,12 +2,19 @@ package Projetos_Java.Projeto_Circulo.src;
 
 
 
-
 public class Circulo{
     double raio;
     String cor;
 
-    public Circulo(double r){
+    Circulo (double r){
         this.raio = r;
+        System.out.println("Raio adicionado ao Circulo.");
     }
+
+    Circulo(double r, String c){
+        this.raio = r;
+        this.cor = c;
+        System.out.println("Racio e cor adicionado ao Circulo.");
+    }
+    
 }
