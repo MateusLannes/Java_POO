6eab1acdbf1fd5e8f6168a4e0ea364a9c8416186
Mateus <mateuslannes100@gmail.com>
2023-03-01 23:09:53 +0000

    Criando Objetos de circulo

--- Projetos_Java/Projeto_Circulo/src/Main.java
+++ Projetos_Java/Projeto_Circulo/src/Main.java
@@ -2,4 +2,12 @@ package Projetos_Java.Projeto_Circulo.src;
 
 public class Main {
     
+
+    Circulo circulo1 = new Circulo(5, "vermelho");
+    Circulo circulo2 = new Circulo(7, "azul");
+
+
+
+    System.out.println("Area do circulo1 e: " + circulo1.calcularArea());
+
 }
