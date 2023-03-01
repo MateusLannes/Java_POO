0fa1cd2890fc40b7100dca2bc5a2bbeb2b74a224
Mateus <mateuslannes100@gmail.com>
2023-03-01 23:13:31 +0000

    Testando metodos de area e perimetro

--- Projetos_Java/Projeto_Circulo/src/Main.java
+++ Projetos_Java/Projeto_Circulo/src/Main.java
@@ -1,13 +1,21 @@
 package Projetos_Java.Projeto_Circulo.src;
 
 public class Main {
+    public static void main(String[] args){
+
+        Circulo circulo1 = new Circulo(5, "vermelho");
+        Circulo circulo2 = new Circulo(7, "azul");
+
+  
+        System.out.println("Circulo1: ");
+        System.out.println("Area: " + circulo1.calcularArea());
+        System.out.println("Perimetro: " + circulo1.calcularPerimetro());
+
+        System.out.println("Circulo2: ");
+        System.out.println("Area: " + circulo2.calcularArea());
+        System.out.println("Perimetro: " + circulo2.calcularPerimetro());
+
+    }
     
-
-    Circulo circulo1 = new Circulo(5, "vermelho");
-    Circulo circulo2 = new Circulo(7, "azul");
-
-
-
-    System.out.println("Area do circulo1 e: " + circulo1.calcularArea());
 
 }
