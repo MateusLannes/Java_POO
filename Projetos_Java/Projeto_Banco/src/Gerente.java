f42f43faf00f3d8ef83148d6fdd22d74088f10dd
Mateus <mateuslannes100@gmail.com>
2023-03-17 13:31:58 +0000

    implementação dos dois metodos validarAcesso

--- Projetos_Java/Projeto_Banco/src/Gerente.java
+++ Projetos_Java/Projeto_Banco/src/Gerente.java
@@ -1,4 +1,5 @@
 package Projetos_Java.Projeto_Banco.src;
+import java.util.Scanner;
 
 /**
  * @author Mateus Lannes Cunha
@@ -18,12 +19,19 @@ public class Gerente extends Pessoa {
         this.senha = senha;
     }
 
-    /* boolean validarAcesso(String s) 
-     * Receberá outra senha como parametro
-     * e vai verificar se ela é correta
-    */
-    
-    /*
-     * boolean validarAcesso()
-     */
+    boolean validarAcesso(String s) {
+        /* valida se a senha s é igual a senha do gerente */
+        return s.equals(this.senha);
+    }
+
+    boolean validarAcesso(){
+        /* Recebe uma senha pelo input e verifica se ela é a do gerente  */
+        Scanner ler = new Scanner(System.in);
+        String senha;
+
+        System.out.printf("Informe a senha: ");
+        senha = ler.nextLine();
+        return (validarAcesso(senha));
+    }
+     
 }
