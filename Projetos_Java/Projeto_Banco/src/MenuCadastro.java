9695081eb5f1c7108ff7214c4299eb2bc76afd8c
Mateus <mateuslannes100@gmail.com>
2023-03-27 22:04:29 +0000

    Correção do import e nas opções

--- Projetos_Java/Projeto_Banco/src/MenuCadastro.java
+++ Projetos_Java/Projeto_Banco/src/MenuCadastro.java
@@ -1,5 +1,7 @@
 package Projetos_Java.Projeto_Banco.src;
 
+import java.util.Scanner;
+
 /**
  * @author Mateus Lannes Cunha
  * data    25/03/2023
@@ -12,11 +14,13 @@ public class MenuCadastro {
     public void imprimeMenu() {
         /* imprime as opcoes do menu do cadastro */
         System.out.println("Escolha uma das opcoes");
-        System.out.println("a) Adicionar pessoa");
-        System.out.println("b) Adicionar gerente");
-        System.out.println("c) Criar conta corrente");
-        System.out.println("d) Criar poupanca");
-        System.out.println("e) Sair");
+        System.out.println("1) Adicionar pessoa");
+        System.out.println("2) Adicionar gerente");
+        System.out.println("3) Criar conta corrente");
+        System.out.println("4) Criar poupanca");
+        System.out.println("0) Sair");
         System.out.printf("Digite aqui: ");
     }
+
+
 }
