1b4ede548e583ceaaa76fa2367ebcc9944154576
Mateus <mateuslannes100@gmail.com>
2023-03-27 21:31:58 +0000

    Implementando metodo de impressão do menu do cadastro

---
+++ Projetos_Java/Projeto_Banco/src/MenuCadastro.java
@@ -0,0 +1,17 @@
+package Projetos_Java.Projeto_Banco.src;
+
+public class MenuCadastro {
+
+    
+
+    public void imprimeMenu() {
+        /* imprime as opcoes do menu */
+        System.out.println("Escolha uma das opcoes");
+        System.out.println("a) Adicionar pessoa");
+        System.out.println("b) Adicionar gerente");
+        System.out.println("c) Criar conta corrente");
+        System.out.println("d) Criar poupanca");
+        System.out.println("e) Sair");
+        System.out.printf("Digite aqui: ");
+    }
+}
